3af07df9d9d85eef408878bf2c13df4903f49def
TanhMai1 <115852235+TanhMai1@users.noreply.github.com>
2023-01-21 02:06:37 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿double firstNumber;
+double secondNumber;
+double result;
+
+Console.WriteLine("Welcome To The Calculator");
+Console.WriteLine("--------------------------");
+
+Console.WriteLine("Enter first number: ");
+firstNumber = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter second number: ");
+secondNumber = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("What operation would you like to do? ");
+Console.WriteLine("+: Addition");
+Console.WriteLine("+: Addition");
+Console.WriteLine("+: Addition");
+Console.WriteLine("+: Addition");
