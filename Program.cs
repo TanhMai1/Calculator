b24179731fa6be211ba1915acb777165a7dd89ea
TanhMai1 <115852235+TanhMai1@users.noreply.github.com>
2023-01-21 02:38:32 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,18 +1,42 @@
-﻿double firstNumber;
-double secondNumber;
-double result;
+do
+{
+    double firstNumber = 0;
+    double secondNumber = 0;
+    double result = 0;
 
-Console.WriteLine("Welcome To The Calculator");
-Console.WriteLine("--------------------------");
+    Console.WriteLine("Welcome To The Calculator");
+    Console.WriteLine("--------------------------");
 
-Console.WriteLine("Enter first number: ");
-firstNumber = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Enter first number: ");
+    firstNumber = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine("Enter second number: ");
-secondNumber = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Enter second number: ");
+    secondNumber = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine("What operation would you like to do? ");
-Console.WriteLine("+: Addition");
-Console.WriteLine("+: Addition");
-Console.WriteLine("+: Addition");
-Console.WriteLine("+: Addition");
+    Console.WriteLine("What operation would you like to do? ");
+    Console.WriteLine("+: Addition");
+    Console.WriteLine("-: Subtraction");
+    Console.WriteLine("*: Multiplication");
+    Console.WriteLine("/: Division");
+
+    switch (Console.ReadLine())
+    {
+        case "+":
+            result = firstNumber + secondNumber;
+            Console.WriteLine($"Result is {firstNumber} + {secondNumber} = {result}");
+            break;
+        case "-":
+            result = firstNumber - secondNumber;
+            Console.WriteLine($"Result is {firstNumber} - {secondNumber} = {result}");
+            break;
+        case "*":
+            result = firstNumber * secondNumber;
+            Console.WriteLine($"Result is {firstNumber} - {secondNumber} = {result}");
+            break;
+        case "/":
+            result = firstNumber / secondNumber;
+            Console.WriteLine($"Result is {firstNumber} / {secondNumber} = {result}");
+            break;
+    }
+    Console.WriteLine("Woud you like to restart? Y/N?");
+} while (Console.ReadLine().ToUpper()=="Y")
